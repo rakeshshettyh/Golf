0af16a6afe30d4cbb0c0d9c71be73f72739a70e7
rakesh <rakesh@Admins-iMac-13.local>
2011-03-21 13:02:43 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamebook::Application.config.secret_token = 'a79874819085efc9683f6d64b017ae38b40c619c522c846b344fb5e618a70967ad92dc23af620498b9641fe27ac7768690d1395c6092b9dfc2740f41ea853f7a'
